416f1f00317434fe64988c4e4ff0c13773d4ce1f
MYMC001 <cherifmohamedou1@gmail.com>
2023-12-30 23:56:45 +0300

    model

--- obj/Debug/net6.0/Hospital_Randevu.AssemblyInfo.cs
+++ obj/Debug/net6.0/Hospital_Randevu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Randevu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b0454dffc3d8c03fae473c3539ea7d1cef574cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Randevu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Randevu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
